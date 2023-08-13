d011699adb27e26266cf533eec233942a5afcfad
karim salazar <karim_salazar@outlook.com>
2023-08-12 22:34:19 -0700

    LetsLinkMyDesing

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //conetected buttonStoryBoard
+       
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView3: UIImageView!
+    @IBOutlet weak var diceImageView4: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       //who          //that    //value
+        diceImageView2.image = UIImage(named: "DiceSix")
+        diceImageView1.image = UIImage(named: "DiceTwo")
+        diceImageView3.image = UIImage(named: "DiceFive")
+        diceImageView4.image = UIImage(named: "DiceThree")
+        
+    
+        
     }
-
-
+   
 }
 
